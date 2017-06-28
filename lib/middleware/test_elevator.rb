50a788d9880faf634a6bb22e6f47103f86654462
Tom Dracz <tjdracz@gmail.com>
2017-06-28 12:05:32 +0100

    Hack

--- lib/middleware/test_elevator.rb
+++ lib/middleware/test_elevator.rb
@@ -1,5 +1,17 @@
 class TestElevator < Apartment::Elevators::Generic
   # @return {String} - The tenant to switch to
+
+  def call(env)
+    if Rails.env == "test" && env["HTTP_USER_AGENT"]&.match?("PhantomJS")
+      request = Rack::Request.new(env)
+      database = @processor.call(request)
+      Apartment::Tenant.switch! database if database
+      @app.call(env)
+    else
+      super
+    end
+  end
+
   def parse_tenant_name(request)
     # request is an instance of Rack::Request
 
